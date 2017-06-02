9c4564e7904174170e384f684b235194c90113a5
Isaac DeMello <idemello@hawaii.edu>
2017-06-01 15:09:30 -1000

    finance attributed populate a list which can be broken down

--- finance.py
+++ finance.py
@@ -13,21 +13,39 @@ import requests
 
 #retrieve web page with our data, parse it using html module ad save results in tree
 
-page = requests.get('https://www.creditunionsonline.com/credit-union-6644.html')
+page = requests.get('https://www.creditunionsonline.com/credit-union-financials-6644.html')
 tree = html.fromstring(page.content)
-address = tree.xpath('//span[@itemprop="streetAddress"]/text()')
-city = tree.xpath('//span[@itemprop="addressLocality"]/text()')
-state = tree.xpath('//span[@itemprop="addressRegion"]/text()')
-zip = tree.xpath('//span[@itemprop="postalCode"]/text()')
-phone = tree.xpath('//div[@class="textH"]/span/text()')
+
+finance = tree.xpath('//div[@class="financeTable"]/div[@class="hoursRow"]/div[@class="hoursCell hoursR"]/text()')
+
+assets = finance[2]
+loans = finance[3]
+netWorth = finance[4]
+members = finance[5]
+fullTime = finance[6]
+partTime = finance[7]
+
+print(assets)
+print(loans)
+print(netWorth)
+print(members)
+print(fullTime)
+print(partTime)
+
+
+#city = tree.xpath('//span[@itemprop="addressLocality"]/text()')
+#state = tree.xpath('//span[@itemprop="addressRegion"]/text()')
+#zip = tree.xpath('//span[@itemprop="postalCode"]/text()')
+#phone = tree.xpath('//div[@class="textH"]/span/text()')
 
 #print to console
-	
-print(address)
-print(city)
-print(state)
-print(zip)
-print(phone)
+
+print(assets)	
+#print(address)
+#print(city)
+#print(state)
+#print(zip)
+#print(phone)
 
 
 #tree now contains the whole HTML file in a nice tree structure
